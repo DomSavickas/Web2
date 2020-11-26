c3bd088697411afa3555d9dc5606100ed07b1f86
Dominykas <dominykas.savickas@knf.stud.vu.lt>
2020-11-26 13:01:11 +0200

    Užbaigta

--- ajax/deleteAdmin.php
+++ ajax/deleteAdmin.php
@@ -1,27 +1,20 @@
 <?php
 session_start();
-$conn = new mysqli('localhost', 'root', '', 'company_db');
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "company_db";
 $id=$_POST['id'];
-$sql="DELETE FROM admin_login WHERE admin_id = $id";
-if ($conn->query($sql) === TRUE) {
+$conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=gbk", $username, $password);
+$conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$sql = $conn->prepare("DELETE FROM admin_login WHERE admin_id = :id");
+$sql->bindParam(':id',$id);
+if ($sql->execute() === TRUE) {
     echo "duomenys ištrinti";
 }
 else
 {
     echo "duomenų nepavyko ištrinti";
 }
-?>
-<?php
-/*$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "company_db";
-$id=$_POST['id'];
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // sql to delete a record
-    $sql = "DELETE FROM admin_login WHERE admin_id = $id";
-
-    // use exec() because no results are returned
-    $conn->exec($sql);
 $conn = null;
-*/?>
+?>
--- ajax/deleteProduct.php
+++ ajax/deleteProduct.php
@@ -1,13 +1,20 @@
 <?php
 session_start();
-$conn = new mysqli('localhost', 'root', '', 'company_db');
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "company_db";
+$conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=gbk", $username, $password);
+$conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 $id=$_POST['id'];
-$sql="DELETE FROM preke WHERE id = $id";
-if ($conn->query($sql) === TRUE) {
+$sql=$conn->prepare("DELETE FROM preke WHERE id = :id");
+$sql->bindParam(':id',$id);
+if ($sql->execute() === TRUE) {
     echo "duomenys ištrinti";
 }
 else
 {
     echo "duomenų nepavyko ištrinti";
 }
+$conn = null;
 ?>
